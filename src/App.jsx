1e16e4d3946fb869cfc4a787b153e351c613e37d
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-09-11 19:25:34 +0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
